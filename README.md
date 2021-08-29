18bb718a90a63822d99785bbe7e5cbc16469f38e
Rahmonjon Ibragimov <rahmonjonibragimov@Rahmonjons-Mac-mini.local>
2021-08-29 19:46:29 +0200

    new updating

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # onlinedatingapp
 Lets build online dating app
+
+I changed in package.json file in scripts from test => to start and in text from echo / error => node server.js 
+so that i can use nodemon instead of node in order to watch it coninously
+now i can write npm start  
+
+npm install --save-dev nodemon => i have installed this one because we have access for npm package
+now i can just write nodemon and hit enter and it restart any time plus watching the proccess
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 // here we write JS to build the server
 
-
 const express = require('express');
-// 
-const app = express();
+const exphbs = require('express-handlebars');
+// those above are for dependencies from package.json
+const app = express(); 
 //
-const port = 3000;
+const port = 3000; 
 // assign port as value
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+// setup view engine method. 
+app.set('view engine', 'handlebars');
+//
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    res.render('home', {
+        title: 'Home'
+    });
+}); 
 // I used get method to handle the route, / means homepage, and req => is incoming request from client, res => is respond from backend to the client
 // res.send method is used to send message to the client
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    });
+});
+
+app.get('/contact', (req, res)=> {
+    res.render('contact', {
+        title: 'Contact'
+    });
+});
+
 app.listen(port,() =>{
     // console.log('Server is running on port' + port); instead of using concatinate, i can use with backtic & dollar sign and inject port in it.
     console.log(`Server is running on port ${port}`);
-});
+}); 
 // app is to used to bring express module. it has listen method and used for port and call back funtion. 
 //  node server.js now go to the localhost:3000 in browser.
